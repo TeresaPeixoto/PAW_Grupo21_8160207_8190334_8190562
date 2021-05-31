5e5db7c4c2207309299dd8eb492f82ee0fa87ec9
Nuno-FM <Nuno-F-O-Matos@hotmail.com>
2021-05-31 18:11:40 +0100

    6th commit, alteração do model user e userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,12 @@
 var mongoose = require("mongoose");
 var User = require("../models/user");
 const jwt = require('jsonwebtoken');
-var authconfig =require('../config/authconfig');
+var authconfig = require('../config/authconfig');
 
 
 var userController = {};
 
 userController.createAccount = function (req, res) {
-  
   var user = new User(req.body);
 
   User.findOne({ email: req.body.email }, function (err, result) {
@@ -18,15 +17,65 @@ userController.createAccount = function (req, res) {
     } else {
       user.save((err) => {
         {
-          if(err){
+          if (err) {
             console.log(err);
-            
-          }else{
-          res.json({registo:true});
-        }}
+          } else {
+            res.json({ registo: true });
+          }
+        }
       });
     }
   });
 };
+
+userController.showById = function (req, res) {
+  User.findOne({ _id: req.params._id }).exec((err, dbitem) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(dbitem);
+    }
+  })
+}
+
+userController.showByEmail = function (req, res) {
+  User.findOne({ email: req.params.email }).exec((err, dbitem) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(dbitem);
+    }
+  })
+}
+
+userController.editById = function (req, res) {
+  User.findByIdAndUpdate(req.body._id, req.body, (err, editedItem) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(editedItem);
+    }
+  })
+}
+
+userController.deleteById = function (req, res) {
+  User.remove({ _id: req.params._id }).exec((err, deletedItems) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(deletedItem);
+    }
+  })
+}
+
+userController.deleteByEmail = function (req, res) {
+  User.remove({ email: req.params.email }).exec((err, deletedItems) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(deletedItem);
+    }
+  })
+}
 
 module.exports = userController;
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,18 +1,26 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-    userName: {
-        type: String,
-        unique: true 
-      },
-    email: {
-        type: String,
-        unique: true 
-      },
-    password: String,
-    birthDate: {type:Date},
-    cellphone: Number,
-    morada: String
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: String,
+  birthDate: { type: Date },
+  cellphone: Number,
+  nif: { 
+    type: Number,
+    unique: true 
+  },
+  address: String,
+  role: { 
+    type: String, 
+    default: 'cliente' 
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
