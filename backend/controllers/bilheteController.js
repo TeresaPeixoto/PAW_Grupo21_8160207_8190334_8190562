2f856d40000cadaf85c19637156d415339e79f8e
Nuno-FM <Nuno-F-O-Matos@hotmail.com>
2021-06-13 01:05:16 +0100

    40th commit - Completado o router e o controller de bilhete

--- backend/controllers/bilheteController.js
+++ backend/controllers/bilheteController.js
@@ -32,4 +32,59 @@ bilheteController.addBilhete = function (req, res) {
     });
 };
 
+bilheteController.editBilheteByID = function (req, res) {
+    Bilhete.findByIdAndUpdate(req.params.id, req.body, (err, updatedEvent) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(updatedEvent);
+        }
+    });
+};
+
+bilheteController.showByID = function (req, res) {
+    Bilhete.findById(req.params.id, (err, dbLocal) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(dbLocal);
+            res.json(dbLocal);
+        }
+    });
+};
+
+bilheteController.showAllBilhetesByUserID = function (req, res) {
+    Bilhete.find({ userID: req.params.userID }, (err, bilhetes) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(bilhetes);
+            res.json(bilhetes);
+        }
+    });
+}
+
+bilheteController.cancelBilhete = function (req, res) {
+    Bilhete.findByIdAndUpdate({ _id: req.params.id },
+        { $set: { ticketStatus: "Cancelado" } },
+        { new: true }).exec(function (err, cancelledBilhete) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(cancelledBilhete);
+                res.json(cancelledBilhete);
+            }
+        });
+};
+
+bilheteController.deleteBilhete = function (req, res) {
+    Bilhete.findByIdAndRemove({ _id: req.params.id }, (err, deletedBilhete) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(deletedBilhete);
+        }
+    });
+};
+
 module.exports = bilheteController;
--- backend/routes/bilhete.js
+++ backend/routes/bilhete.js
@@ -7,4 +7,14 @@ var bilheteController = require("../controllers/bilheteController");
 
 router.post("/:id", bilheteController.addBilhete);
 
+router.put('/edit/:id', bilheteController.editBilheteByID);
+
+router.get('/view/:id', bilheteController.showByID);
+
+router.get('/viewtickets/:userID', bilheteController.showAllBilhetesByUserID);
+
+router.put('/cancelticket/:id', bilheteController.cancelBilhete);
+
+router.delete('/delete/:id', bilheteController.deleteBilhete);
+
 module.exports = router;
